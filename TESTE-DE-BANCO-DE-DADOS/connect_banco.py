9b8fba369983e3bdae4dc0d741c8c681ec953d94
Francisco Fernando <nando32117@gmail.com>
2025-03-31 20:23:39 -0300

    Alteração na estrutura das tabelas operadoras e despesas

--- TESTE-DE-BANCO-DE-DADOS/connect_banco.py
+++ TESTE-DE-BANCO-DE-DADOS/connect_banco.py
@@ -59,27 +59,26 @@ try:
     # SQL para criar a tabela operadoras se não existir  
     create_operadoras_table_query = '''  
     CREATE TABLE IF NOT EXISTS operadoras (  
-        id SERIAL PRIMARY KEY,  
-        registro_ans VARCHAR(20) UNIQUE,  
-        cnpj VARCHAR(14),   
-        razao_social VARCHAR(255),  
-        nome_fantasia VARCHAR(255),  
-        modalidade VARCHAR(50),  
-        logradouro VARCHAR(100),  
-        numero VARCHAR(15),  
-        complemento VARCHAR(50),  
-        bairro VARCHAR(50),  
-        cidade VARCHAR(50),  
-        uf VARCHAR(2),  
-        cep VARCHAR(10),  
-        ddd VARCHAR(3),  
-        telefone VARCHAR(20),  
-        fax VARCHAR(20),  
-        endereco_eletronico VARCHAR(100),  
-        representante VARCHAR(100),  
-        cargo_representante VARCHAR(100),  
-        regiao_de_comercializacao VARCHAR(50),  
-        data_registro_ans DATE  
+        registro_operadora VARCHAR(6) PRIMARY KEY,
+        cnpj VARCHAR(14) UNIQUE NOT NULL,
+        razao_social VARCHAR(140),
+        nome_fantasia VARCHAR(140),
+        modalidade VARCHAR(50),
+        logradouro VARCHAR(40),
+        numero VARCHAR(20),
+        complemento VARCHAR(40),
+        bairro VARCHAR(30),
+        cidade VARCHAR(30),
+        uf CHAR(2),
+        cep VARCHAR(8),
+        ddd VARCHAR(4),
+        telefone VARCHAR(20),
+        fax VARCHAR(20),
+        endereco_eletronico VARCHAR(255),
+        representante VARCHAR(50),
+        cargo_representante VARCHAR(40),
+        regiao_de_comercializacao SMALLINT CHECK (regiao_de_comercializacao BETWEEN 1 AND 6),
+        data_registro_ans DATE NOT NULL
     );  
     '''  
     # Executar a query de criação de tabela operadoras  
@@ -96,40 +95,43 @@ try:
         header = next(reader)  # Ignorar o cabeçalho  
 
         for row in reader:  
-            # Lê os dados do CSV e atribui à variável correspondente  
-            registro_ans = row[0] if row[0] else None  
-            cnpj = row[1] if row[1] else None  
-            razao_social = row[2] if row[2] else None  
-            nome_fantasia = row[3] if row[3] else None  
-            modalidade = row[4] if row[4] else None  
-            logradouro = row[5] if row[5] else None  
-            numero = row[6] if row[6] else None  
-            complemento = row[7] if row[7] else None  
-            bairro = row[8] if row[8] else None  
-            cidade = row[9] if row[9] else None  
-            uf = row[10] if row[10] else None  
-            cep = row[11] if row[11] else None  
-            ddd = row[12] if row[12] else None  
-            telefone = row[13] if row[13] else None  
-            fax = row[14] if row[14] else None  
-            endereco_eletronico = row[15] if row[15] else None  
-            representante = row[16] if row[16] else None  
-            cargo_representante = row[17] if row[17] else None  
-            regiao_de_comercializacao = row[18] if row[18] else None  
-            data_registro_ans = row[19] if row[19] else None  
-            
-            # Inserir os dados na tabela  
-            cursor.execute('''  
-                INSERT INTO operadoras (registro_ans, cnpj, razao_social, nome_fantasia, modalidade,  
-                                        logradouro, numero, complemento, bairro, cidade, uf, cep,  
-                                        ddd, telefone, fax, endereco_eletronico, representante,  
-                                        cargo_representante, regiao_de_comercializacao, data_registro_ans)  
+            # Lê os dados do CSV e converte valores vazios para None
+            registro_operadora = row[0] if row[0] else None
+            cnpj = row[1] if row[1] else None
+            razao_social = row[2] if row[2] else None
+            nome_fantasia = row[3] if row[3] else None
+            modalidade = row[4] if row[4] else None
+            logradouro = row[5] if row[5] else None
+            numero = row[6] if row[6] else None
+            complemento = row[7] if row[7] else None
+            bairro = row[8] if row[8] else None
+            cidade = row[9] if row[9] else None
+            uf = row[10] if row[10] else None
+            cep = row[11] if row[11] else None
+            ddd = row[12] if row[12] else None
+            telefone = row[13] if row[13] else None
+            fax = row[14] if row[14] else None
+            endereco_eletronico = row[15] if row[15] else None
+            representante = row[16] if row[16] else None
+            cargo_representante = row[17] if row[17] else None
+            regiao_de_comercializacao = int(row[18]) if row[18] else None
+                    
+            # Convertendo data para formato PostgreSQL (YYYY-MM-DD)
+            data_registro_ans = row[19] if row[19] else None
+            if data_registro_ans:
+                data_registro_ans = data_registro_ans.replace('/', '-')  # Substitui '/' por '-'
+        
+            # Inserir os dados na tabela
+            cursor.execute('''
+                INSERT INTO operadoras (registro_operadora, cnpj, razao_social, nome_fantasia, modalidade,  
+                                       logradouro, numero, complemento, bairro, cidade, uf, cep,  
+                                       ddd, telefone, fax, endereco_eletronico, representante,  
+                                       cargo_representante, regiao_de_comercializacao, data_registro_ans)  
                 VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-                ON CONFLICT (cnpj) DO NOTHING 
-            ''', (  
-                registro_ans, cnpj, razao_social, nome_fantasia, modalidade, logradouro, numero, complemento, bairro, cidade, uf, cep,  
-                ddd, telefone, fax, endereco_eletronico, representante,  
-                cargo_representante, regiao_de_comercializacao, data_registro_ans  
+                ON CONFLICT (registro_operadora) DO NOTHING
+            ''', (
+                registro_operadora, cnpj, razao_social, nome_fantasia, modalidade, logradouro, numero, complemento, bairro, cidade, uf, cep,
+                ddd, telefone, fax, endereco_eletronico, representante, cargo_representante, regiao_de_comercializacao, data_registro_ans
             ))  
 
     conn.commit()  
@@ -138,13 +140,12 @@ try:
     # SQL para criar a tabela despesas se não existir  
     create_despesas_table_query = '''  
     CREATE TABLE IF NOT EXISTS despesas (  
-        id SERIAL PRIMARY KEY,  
-        data DATE,  
-        reg_ans VARCHAR(20),  
-        cd_conta_contabil VARCHAR(255),  
-        descricao VARCHAR(255),  
-        vl_saldo_inicial DECIMAL(15, 2),   
-        vl_saldo_final DECIMAL(15, 2) 
+        data DATE NOT NULL,
+        reg_ans BIGINT NOT NULL,
+        cd_conta_contabil BIGINT NOT NULL,
+        descricao VARCHAR(150) NOT NULL,
+        vl_saldo_inicial NUMERIC(15, 2),
+        vl_saldo_final NUMERIC(15, 2) 
     );  
     ''' 
     # Executar a query de criação de tabela despesas  
